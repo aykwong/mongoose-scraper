02fc357d59c6433d99d8d6e442a0ce1f72d7f8c7
Kwong <aykwong93@gmail.com>
2018-10-03 23:12:19 -0400

    Index routes WIP

--- app.js
+++ app.js
@@ -1,47 +1,29 @@
-var createError = require('http-errors');
 var express = require('express');
+var bodyParser = require("body-parser");
 var logger = require("morgan");
 var path = require('path');
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 
 // mongoose connect
 mongoose.connect("mongodb://localhost/mongooseScraper", { useNewUrlParser: true });
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var PORT = 8080;
-
 var app = express();
+var PORT = process.env.PORT || 8080;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// BodyParser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// handlebars setup
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+require("./routes/index.js")(app);
 
 // Start the server
 app.listen(PORT, function() {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,69 @@
-var express = require('express');
-var router = express.Router();
+var request = require("request");
+var cheerio = require("cheerio");
+var db = require("../models");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+module.exports = function (app) {
+  app.get('/scrape', function (req, res) {
+    request("http://www.echojs.com/", function (err, response, html) {
+
+      var $ = cheerio.load(response.body);
+
+      var result = {};
+
+      $("article h2").each(function (i, element) {
+
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(element).children("a").text();
+        result.link = $(element).children("a").attr("href");
+
+        console.log(result);
+
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function (dbArticle) {
+            console.log(dbArticle);
+          })
+          .catch(function (err) {
+            return res.json(err);
+          });
+      });
+      console.log("Scraping complete!");
+    });
+  });
+
+  app.get('/newArticles', function (req, res) {
+    // Save a new entry using the data object
+    db.Article.find({})
+      .then(function (dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function (err) {
+        res.json(err);
+      });
+  });
+};
+
+app.get("/Articles/:id", function(req, res) {
+  db.Article.findOne({ _id: req.params.id })
+    .populate("note")
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
 });
 
-module.exports = router;
+app.post("/Articles/:id", function(req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function(dbNote) {
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function(dbArticle) {
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
